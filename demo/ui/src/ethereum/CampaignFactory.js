bb96a70f8772e397d2b69d561293364f330a0b87
15077693d <15077693d@gmail.com>
2021-06-06 22:47:33 +0800

    add readme document

--- demo/ui/src/ethereum/CampaignFactory.js
+++ demo/ui/src/ethereum/CampaignFactory.js
@@ -1,7 +1,7 @@
 import {getAccount, web3} from './web3.js'
 import CampaignFactoryJson from './CampaignFactory.json'
 
-let campaignFactoryAddress = "0x3CFa772d4167D4079b457Ab7ff0C93C8575864a5"
+let campaignFactoryAddress = "0xCac12b7BD4422a6a9a103F8c0fe35C9fBe763BC7"
 let campaignFactoryInstance = new web3.eth.Contract(CampaignFactoryJson.abi, campaignFactoryAddress)
 
 const addCampaign = async (baseURI,
